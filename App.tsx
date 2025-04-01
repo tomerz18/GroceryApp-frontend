1b977e21186bf62b32ad7125eceef46186001d7e
Tomer Zadok <tomerz18@gmail.com>
2025-04-02 01:23:12 +0300

    Fetch the groceries data from the API

--- App.tsx
+++ App.tsx
@@ -1,14 +1,48 @@
-import React, { useState } from 'react';
-import { Text, View, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TextInput, FlatList, TouchableOpacity, Alert } from 'react-native';
 import styles from './src/styles';
+import axios from 'axios';
+
+const API_URL = 'https://mznq30wkg3.execute-api.us-east-1.amazonaws.com';
+
+interface GroceryItem {
+  name: string;
+}
 
 const App = () => {
   const [grocery, setItem] = useState('');
-  const [groceryList, setGroceryList] = useState<string[]>([]);
+  const [groceryList, setGroceryList] = useState<GroceryItem[]>([]);
+
+
+  const getGroceryItems = async (): Promise<GroceryItem[]> => {
+    try {
+      const response = await axios.get(`${API_URL}/items`);
+      console.log(response)
+      return response.data;
+    } catch (error) {
+      console.error('Failed to fetch grocery items:', error);
+      throw error;
+    }
+  };
+
+    // Fetch grocery items when the app loads
+    useEffect(() => {
+      fetchGroceryItems();
+    }, []);
+
+  const fetchGroceryItems = async () => {
+    try {
+      const items = await getGroceryItems();
+      setGroceryList(items);
+    } catch (error) {
+      Alert.alert('Error', 'Failed to load grocery items');
+      console.error(error);
+    }
+  };
 
   const addItemToList = () => {
     if (grocery) {
-      setGroceryList([...groceryList, grocery]);
+      setGroceryList([...groceryList, { name: grocery }]);
       setItem(''); // Clear input field after adding
     }
   };
@@ -37,7 +71,7 @@ const App = () => {
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item, index }) => (
           <View style={styles.listItem}>
-            <Text style={styles.listText}>{item}</Text>
+            <Text style={styles.listText}>{item.name}</Text>
             <TouchableOpacity onPress={() => removeItemFromList(index)}>
               <Text style={styles.removeText}>הסר</Text>
             </TouchableOpacity>
